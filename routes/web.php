ff5d6f1325edc6e7ae3ea161d4d9b9464f753b01
JENNY <48593166+jenny111788@users.noreply.github.com>
2021-03-31 16:12:52 +0800

    Day22 專案練習 Part2-文章功能

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticlesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::resource('articles', ArticlesController::class);
+
+Route::get('/', [ArticlesController::class, 'index'])->name('root');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
